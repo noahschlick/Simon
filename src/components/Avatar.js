cccc245c54237f448f8f35ef147059cf0041f5b6
noahschlick <49171976+noahschlick@users.noreply.github.com>
2024-01-02 12:40:47 -0600

    Fix Avatars

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -8,7 +8,7 @@ function Avatar({seed, large}) {
   return (
     <div className={`overflow-hidden relative ${large ? 'h-20 w-20' : 'h-10 w-10'} 
             rounded-full border-gray-300 bg-white my-auto`}>
-        <img src={`https://avatars.dicebear.com/api/open-peeps/${
+        <img src={`https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=${
             seed || 'Hank'
         }.svg`} alt=""/>
     </div>
